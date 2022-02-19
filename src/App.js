9a746415cd2a8d5d6b755e60a7f94cdcbf367d62
Fernando Coutinho <fa2c1@hotmail.com>
2022-02-19 17:06:52 -0300

    Aprendendo useStates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de Luz',
+    'Estudar React Hooks'
+  ]);
+
+  const [input, setInput] = useState('');
+
+  function handleAdd(){
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }
+
   return (
     <div>
-      <h1>Usando os Hooks</h1>
+      <h1>Lista de Tarefas</h1>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+      <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
